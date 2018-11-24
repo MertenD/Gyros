123729391eac0d53a8f27d4dcac27c0c4a6734a7
nlpke <nlpke@users.noreply.github.com>
2018-11-25 00:04:41 +0100

    Add Webserver

---
+++ GyrosComputerInterface/src/Webserver.java
@@ -0,0 +1,26 @@
+import com.sun.net.httpserver.*;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+
+
+public class Webserver { 
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/test", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "<h1><marquee>Test</marquee></h1>";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
